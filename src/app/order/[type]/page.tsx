85788b0a04f26351c59dd8982984608d797797e4
nilberm <nilber16@outlook.com>
2025-06-01 21:57:14 -0300

    fix: ajust params

--- src/app/order/[type]/page.tsx
+++ src/app/order/[type]/page.tsx
@@ -1,20 +1,22 @@
-import OrderPage from "@/components/OrderPage";
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { drawingTypes } from "@/data/drawingTypes";
+import OrderPage from "@/components/OrderPage";
 
-interface Params {
+export default function OrderTypePage({
+  params,
+  searchParams,
+}: {
   params: { type: string };
-}
-
-export default function OrderTypePage({ params }: Params) {
+  searchParams: Record<string, string | string[] | undefined>;
+}) {
   const { type } = params;
-
   const drawing = drawingTypes.find((dt) => dt.slug === type);
 
   if (!drawing) {
     return (
       <div className="min-h-screen flex items-center justify-center p-4">
         <h2 className="text-xl font-semibold text-red-600">
-          Tipo de desenho `&quot;`{type}`&quot;` não encontrado.
+          Tipo de desenho &quot;{type}&quot; não encontrado.
         </h2>
       </div>
     );
